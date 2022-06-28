13ad70fb7f193f34f26fbbc730a498439535adbb
su-hub9 <su9581@gmail.com>
2022-06-28 21:05:36 +0900

    style: 상수와 단락 구분

--- src/main/java/nextstep/subway/fare/domain/Fare.java
+++ src/main/java/nextstep/subway/fare/domain/Fare.java
@@ -3,6 +3,7 @@ package nextstep.subway.fare.domain;
 public class Fare {
     private static final String NOT_LESS_THAN_ZERO_FARE = "요금은 0원 보다 적을 수 없습니다.";
     private static final int FREE = 0;
+
     private final int fare;
     private Fare(int fare) {
         this.fare = fare;
