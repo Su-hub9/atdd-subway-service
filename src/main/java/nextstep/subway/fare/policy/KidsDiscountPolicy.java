53006258529ff029547dc9d1f61e3ddca547e11c
su-hub9 <su9581@gmail.com>
2022-06-28 21:06:34 +0900

    refactor(policy): 발생되지 않을 케이스에 대한 로직 수정

--- src/main/java/nextstep/subway/fare/policy/KidsDiscountPolicy.java
+++ src/main/java/nextstep/subway/fare/policy/KidsDiscountPolicy.java
@@ -3,12 +3,11 @@ package nextstep.subway.fare.policy;
 import nextstep.subway.fare.domain.Fare;
 
 public class KidsDiscountPolicy implements DiscountPolicy {
-    private static final int BASIC_FARE = 1250;
     private static final int DEDUCTION_FARE = 350;
 
     @Override
     public Fare discount(int fare) {
-        return Fare.from(Math.max(calculateFare(fare), calculateFare(BASIC_FARE)));
+        return Fare.from(calculateFare(fare));
     }
 
     private int calculateFare(int fare) {
--- src/main/java/nextstep/subway/fare/policy/NoneDiscountPolicy.java
+++ src/main/java/nextstep/subway/fare/policy/NoneDiscountPolicy.java
@@ -3,9 +3,8 @@ package nextstep.subway.fare.policy;
 import nextstep.subway.fare.domain.Fare;
 
 public class NoneDiscountPolicy implements DiscountPolicy {
-    private static final int BASIC_FARE = 1250;
     @Override
     public Fare discount(int fare) {
-        return Fare.from(Math.max(fare, BASIC_FARE));
+        return Fare.from(fare);
     }
 }
